39c58a457c92c45d3659d6b307922c353afd02e6
hadirkhan10 <hadirkhan10@gmail.com>
2020-12-09 08:39:37 +0000

    Created a new test program

--- verilog/dv/caravel/user_proj_example/ibtida/ibtida_test.c
+++ verilog/dv/caravel/user_proj_example/ibtida/ibtida_test.c
@@ -10,7 +10,6 @@ void main()
   reg_mprj_io_5 = GPIO_MODE_USER_STD_INPUT_NOPULL;
   // reg_mprj_io_6 = GPIO_MODE_USER_STD_OUTPUT;
   // reg_mprj_io_7 = GPIO_MODE_USER_STD_OUTPUT;
-  reg_mprj_io_36 = GPIO_MODE_USER_STD_OUTPUT;
   reg_mprj_io_37 = GPIO_MODE_MGMT_STD_OUTPUT;
 
   reg_mprj_xfer = 1;
@@ -24,11 +23,6 @@ void main()
   reg_la0_data = 0x00000000;
   reg_mprj_datah = 0x20;
   reg_mprj_datah = 0x0;
-  reg_mprj_io_37 = GPIO_MODE_USER_STD_OUTPUT;
-
-  reg_mprj_xfer = 1;
-  while(reg_mprj_xfer == 1);
-
   
 
 }
