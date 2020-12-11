be4d0b7fdfee6aef4ae06e1817d5862cbd379b59
hadirkhan10 <hadirkhan10@gmail.com>
2020-12-11 09:11:19 +0000

    Included the netlist in caravel to simulate it

--- verilog/rtl/caravel.v
+++ verilog/rtl/caravel.v
@@ -62,8 +62,8 @@
 /*------------------------------*/
 /* Include user project here	*/
 /*------------------------------*/
-`include "ibtida-soc/Ibtida_top_dffram_cv.v"
-//`include "../gl/Ibtida_top_dffram_cv.v"
+//`include "ibtida-soc/Ibtida_top_dffram_cv.v"
+`include "../gl/Ibtida_top_dffram_cv.v"
 
 // `ifdef USE_OPENRAM
 //     `include "sram_1rw1r_32_256_8_sky130.v"
