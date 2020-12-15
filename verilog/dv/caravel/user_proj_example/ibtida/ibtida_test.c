967978c444a3efdbfb8d5aa5898d7f5ab4905a39
hadirkhan10 <hadirkhan10@gmail.com>
2020-12-15 06:30:20 +0000

    Changed the test program for further verification

--- verilog/dv/caravel/user_proj_example/ibtida/ibtida_test.c
+++ verilog/dv/caravel/user_proj_example/ibtida/ibtida_test.c
@@ -2,15 +2,14 @@
 
 void main()
 {
-  // reg_mprj_io_0 = GPIO_MODE_USER_STD_OUTPUT;
-   reg_mprj_io_8 = GPIO_MODE_USER_STD_OUTPUT;
-  // reg_mprj_io_2 = GPIO_MODE_USER_STD_OUTPUT;
-  // reg_mprj_io_3 = GPIO_MODE_USER_STD_OUTPUT;
-  // reg_mprj_io_4 = GPIO_MODE_USER_STD_OUTPUT;
-  reg_mprj_io_5 = GPIO_MODE_USER_STD_INPUT_NOPULL;
-  // reg_mprj_io_6 = GPIO_MODE_USER_STD_OUTPUT;
-  // reg_mprj_io_7 = GPIO_MODE_USER_STD_OUTPUT;
-  reg_mprj_io_37 = GPIO_MODE_MGMT_STD_OUTPUT;
+ 
+   reg_mprj_io_8  = GPIO_MODE_USER_STD_OUTPUT;
+   reg_mprj_io_9  = GPIO_MODE_USER_STD_OUTPUT;
+   reg_mprj_io_10 = GPIO_MODE_USER_STD_OUTPUT;
+   reg_mprj_io_11 = GPIO_MODE_USER_STD_OUTPUT;
+
+   reg_mprj_io_5 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+   reg_mprj_io_37 = GPIO_MODE_MGMT_STD_OUTPUT;
 
   reg_mprj_xfer = 1;
   while(reg_mprj_xfer == 1);
