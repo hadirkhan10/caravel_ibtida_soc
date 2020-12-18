ecb3494e74d988a1fd156b46efb53e6970a4e1de
hadirkhan10 <hadirkhan10@gmail.com>
2020-12-18 20:15:13 +0000

    Final run with 0 drc/lvs violations with mpw-one-b branch

--- openlane/user_project_wrapper/interactive.tcl
+++ openlane/user_project_wrapper/interactive.tcl
@@ -1,8 +1,7 @@
 package require openlane
-
 set script_dir [file dirname [file normalize [info script]]]
 
-prep -design $script_dir -tag user_project_wrapper -overwrite
+prep -design $script_dir -tag user_project_wrapper_18_dec_9_pm_core 
 set save_path $script_dir/../..
 
 verilog_elaborate
@@ -11,49 +10,17 @@ init_floorplan
 
 place_io_ol
 
-set ::env(FP_DEF_TEMPATE) $script_dir/../../def/user_project_wrapper_empty.def
-
-apply_def_template
-
 add_macro_placement mprj 660 960 N
 
 manual_macro_placement f
 
-set ::env(_SPACING) 1.7
-set ::env(_WIDTH) 3
-
-#set power_domains [list {vccd1 vssd1} {vccd2 vssd2} ]
-
-set power_domains [list {vccd1 vssd1}]
-#set power_domains [list {vccd1 vssd1} {vccd2 vssd2} {vdda1 vssa1} {vdda2 vssa2}]
-set ::env(_VDD_NET_NAME) vccd1
-set ::env(_GND_NET_NAME) vssd1
-set ::env(_V_OFFSET) 14
-set ::env(_H_OFFSET) $::env(_V_OFFSET)
-set ::env(_V_PITCH) 180
-set ::env(_H_PITCH) 180
-set ::env(_V_PDN_OFFSET) 0
-set ::env(_H_PDN_OFFSET) 0
-
-foreach domain $power_domains {
-	set ::env(_VDD_NET_NAME) [lindex $domain 0]
-	set ::env(_GND_NET_NAME) [lindex $domain 1]
-	gen_pdn
-
-	set ::env(_V_OFFSET) \
-	[expr $::env(_V_OFFSET) + 2*($::env(_WIDTH)+$::env(_SPACING))]
-	set ::env(_H_OFFSET) \
-	[expr $::env(_H_OFFSET) + 2*($::env(_WIDTH)+$::env(_SPACING))]
-	set ::env(_V_PDN_OFFSET) [expr $::env(_V_PDN_OFFSET)+6*$::env(_WIDTH)]
-	set ::env(_H_PDN_OFFSET) [expr $::env(_H_PDN_OFFSET)+6*$::env(_WIDTH)]
-}
+exec -ignorestderr openroad -exit $script_dir/gen_pdn.tcl
+set_def $::env(pdn_tmp_file_tag).def
 
 global_routing_or
 detailed_routing
-
 write_powered_verilog -power vccd1 -ground vssd1
 set_netlist $::env(lvs_result_file_tag).powered.v
-
 run_magic
 run_magic_spice_export
 
@@ -61,8 +28,7 @@ save_views       -lef_path $::env(magic_result_file_tag).lef \
                  -def_path $::env(tritonRoute_result_file_tag).def \
                  -gds_path $::env(magic_result_file_tag).gds \
                  -mag_path $::env(magic_result_file_tag).mag \
-                 -verilog_path $::env(CURRENT_NETLIST) \
-		 -save_path $save_path \
+                 -save_path $save_path \
                  -tag $::env(RUN_TAG)
 
 run_magic_drc
@@ -70,4 +36,3 @@ run_magic_drc
 run_lvs; # requires run_magic_spice_export
 
 run_antenna_check
-
--- openlane/user_project_wrapper/pdn.tcl
+++ openlane/user_project_wrapper/pdn.tcl
@@ -2,7 +2,7 @@
 set ::power_nets $::env(_VDD_NET_NAME)
 set ::ground_nets $::env(_GND_NET_NAME)
 
-pdngen::specify_grid stdcell {
+set stdcell {
     name grid
 	core_ring {
 		met5 {width $::env(_WIDTH) spacing $::env(_SPACING) core_offset $::env(_H_OFFSET)}
@@ -10,32 +10,33 @@ pdngen::specify_grid stdcell {
 	}
 	rails {
 	}
-    straps {
-	    met4 {width $::env(_WIDTH) pitch $::env(_V_PITCH) offset $::env(_V_PDN_OFFSET)}
-	    met5 {width $::env(_WIDTH) pitch $::env(_H_PITCH) offset $::env(_H_PDN_OFFSET)}
-    }
     connect {{met4 met5}}
 }
 
+if { $::env(_WITH_STRAPS) } {
+	dict append stdcell straps {
+	    met4 {width $::env(_WIDTH) pitch $::env(_V_PITCH) offset $::env(_V_PDN_OFFSET)}
+	    met5 {width $::env(_WIDTH) pitch $::env(_H_PITCH) offset $::env(_H_PDN_OFFSET)}
+    }
+}
+
+pdngen::specify_grid stdcell $stdcell
+
 pdngen::specify_grid macro {
-    instance "mprj"
+	instance "mprj"
     power_pins "VPWR"
     ground_pins "VGND"
-    #power_pins $::env(_VDD_NET_NAME)
-    #ground_pins $::env(_GND_NET_NAME)
     blockages "li1 met1 met2 met3 met4"
     straps { 
     } 
     connect {{met4_PIN_ver met5}}
-
 }
 
 
-set ::halo 0
+set ::halo 10
 
 # POWER or GROUND #Std. cell rails starting with power or ground rails at the bottom of the core area
 set ::rails_start_with "POWER" ;
 
 # POWER or GROUND #Upper metal stripes starting with power or ground rails at the left/bottom of the core area
 set ::stripes_start_with "POWER" ;
-
